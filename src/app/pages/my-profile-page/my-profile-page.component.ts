4ef3c013927ebadec0d384aa852c3ba97b8c7776
Jilvan Junior <jilvanj@hotmail.com>
2024-11-24 18:32:45 -0300

    tela produto anuncio

---
+++ src/app/pages/my-profile-page/my-profile-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-profile-page',
+  templateUrl: './my-profile-page.component.html',
+  styleUrl: './my-profile-page.component.css'
+})
+export class MyProfilePageComponent {
+
+}
